e0f29b65833b6dcc0181d3a0bded1463d1fae4f2
Isti0105 <horvatska01@gmail.com>
2022-08-30 21:30:45 +0200

    Make main cleaner

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,23 @@
 #include "glCraft.h"
 
 int main(int argc, char **argv) {
-  std::string savePath = argc > 1 ? argv[1] : "default.glc";
-
-  int result;
-  START_TRACE("startup-trace.json");
-  {
-    Application app;
-    END_TRACE();
-
-    START_TRACE("scene-creation-trace.json");
-
-    app.setScene(std::make_shared<Scene>(savePath));
-    END_TRACE();
-
-    START_TRACE("runtime-trace.json");
-    result = app.run();
-  }
+  START_TRACE("startup.json");
+  auto *app = new Application;
   END_TRACE();
+
+  START_TRACE("scene-creation.json");
+  std::string savePath = argc > 1 ? argv[1] : "default.glc";
+  app->setScene(std::make_shared<Scene>(savePath));
+  END_TRACE();
+
+  START_TRACE("runtime.json");
+  int result;
+  result = app->run();
+  END_TRACE();
+
+  START_TRACE("shutdown.json");
+  delete app;
+  END_TRACE();
+
   return result;
 }
